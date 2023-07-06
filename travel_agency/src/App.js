b700773457909c922ca5cc7ed6a4beef43924d3d
RapidKS <sharma.kaush120375@gmail.com>
2023-07-06 21:49:36 +0530

    new

--- travel_agency/src/App.js
+++ travel_agency/src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import SignUp from './Components/SignUp';
 import Login from './Components/Login';
 
 function App() {
+  const [currForm, setCurrForm] = useState('login');
+
+  const toggleForm = (formName) =>{
+    setCurrForm(formName);
+  }
   return (
     <div className="App">
-     <Login/>
+      {
+        currForm === "login" ? <Login onFormSwitch={toggleForm}/> : <SignUp onFormSwitch={toggleForm}/>
+      }
     </div>
   );
 }
